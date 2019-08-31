d7e39a456145d2b015579d4e6ba31d54fc85d722
Eddic Code <eddic.code@gmail.com>
2019-08-30 21:36:21 -0600

    Removed unnecessary conditional.

--- Yggdrasil.Scripting/BaseConditional.cs
+++ Yggdrasil.Scripting/BaseConditional.cs
@@ -5,11 +5,6 @@
         public abstract bool Execute(object baseState);
     }
 
-    public abstract class BaseConditional<T>
-    {
-        public abstract bool Execute(T state);
-    }
-
     public abstract class BaseDynamicConditional
     {
         public abstract bool Execute(dynamic state);
